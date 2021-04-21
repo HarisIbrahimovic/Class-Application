ade23127e4a36ee9613f06af04135bf296944674
HarisIbrahimovic <81588970+HarisIbrahimovic@users.noreply.github.com>
2021-04-21 12:20:00 +0200

    Login system
    Added sign up and login activities.

--- app/src/main/java/com/example/classapplication/MainActivity.java
+++ app/src/main/java/com/example/classapplication/MainActivity.java
@@ -1,8 +1,24 @@
 package com.example.classapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.classapplication.SignIn.loginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +26,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(getApplicationContext(), loginActivity.class));
+
+
     }
 }
